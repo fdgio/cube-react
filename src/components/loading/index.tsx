60e708daaedd21db53933b603f81db41c9c2b882
huruji <594613537@qq.com>
2018-11-29 02:13:38 +0800

    feat: :sparkles: loading组件

---
+++ src/components/loading/index.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import './index.styl';
+
+export interface LoadingProps {
+	size?: number;
+}
+
+class Button extends React.Component<LoadingProps, any> {
+	render() {
+		const { size } = this.props;
+		let style = {};
+		if (size) {
+			style = {
+				width: `${size}px`,
+				height: `${size}px`
+			};
+		}
+		return (
+			<div className="cube-loading">
+				<span className="cube-loading-spinners" style={style}>
+					{new Array(12).map(() => <i className="cube-loading-spinner" />)}
+				</span>
+			</div>
+		);
+	}
+}
+
+export default Button;
