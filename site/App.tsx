d719559be1d545c5b33e70ebb59972ff4d389548
huruji <594613537@qq.com>
2018-11-29 15:07:35 +0800

    docs: :memo: document site first run

---
+++ site/App.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default class App extends React.Component<{}, any> {
+	render() {
+		return <header>cube-react</header>;
+	}
+}
---
+++ site/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.styl';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.querySelector('#app'));
