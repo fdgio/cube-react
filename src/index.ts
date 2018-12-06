c5315eab2ec6e0b31b83dcc546c8c810f2907d52
huruji <594613537@qq.com>
2018-12-07 01:51:48 +0800

    feat: :sparkles: export popup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import CubeButton from './components/button';
 import CubeLoading from './components/loading';
+import CubePopup from './components/popup';
 
 const VERSION = '0.0.1';
 console.log(VERSION);
-export { VERSION, CubeButton, CubeLoading };
-export default {
-	CubeButton
-};
+export { VERSION, CubeButton, CubeLoading, CubePopup };
