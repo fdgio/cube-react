9a067a5cde816f7c55891415bb44d32f21378717
huruji <594613537@qq.com>
2018-12-11 22:57:24 +0800

    feat: :sparkles: export tip

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import CubeButton from './components/button';
 import CubeLoading from './components/loading';
 import CubePopup from './components/popup';
+import CubeTip from './components/tip';
 
 const VERSION = '0.0.1';
 console.log(VERSION);
-export { VERSION, CubeButton, CubeLoading, CubePopup };
+export { VERSION, CubeButton, CubeLoading, CubePopup, CubeTip };
