a00328d1daebd9a09822acaffed80500474b0a40
huruji <594613537@qq.com>
2018-12-20 01:03:18 +0800

    feat: :sparkles: update input

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -14,10 +14,15 @@ export interface BasicProps {
 	eye?: boolean | object;
 }
 
-export default class Input extends React.Component<BasicProps, any> {
+type InputProps = React.InputHTMLAttributes<{}> & BasicProps;
+
+export default class Input extends React.Component<InputProps, any> {
+	private inputRef: React.RefObject<HTMLInputElement>;
+
 	constructor(props) {
 		super(props);
 		this.state = {
+			inputValue: this.props.value || '',
 			isFocus: false,
 			formatedClearable: {
 				visible: false,
@@ -28,6 +33,7 @@ export default class Input extends React.Component<BasicProps, any> {
 				reverse: false
 			}
 		};
+		this.inputRef = React.createRef();
 	}
 
 	static defaultProps = {
@@ -37,14 +43,47 @@ export default class Input extends React.Component<BasicProps, any> {
 		autofocus: false,
 		autocomplete: false,
 		clearable: false,
-		eye: false
+		eye: false,
+		onFocus: () => {},
+		onBlue: () => {}
 	};
 
+	handleFocus(e) {
+		this.props.onFocus(e);
+		this.setState({
+			isFocus: true
+		});
+	}
+
+	handleBlur(e) {
+		this.props.onBlur(e);
+		this.setState({
+			isFocus: false
+		});
+	}
+
+	handleClear(e) {
+		this.setState({
+			inputValue: ''
+		});
+		this.inputRef.current.focus();
+	}
+
 	render() {
+		const inputClass = classnames({
+			'cube-input_active': this.state.isFocus,
+			'cube-input': true
+		});
+
+		const { children } = { ...this.props };
+		let prepend;
+		if (children[0]) {
+			prepend = <div className="cube-input-prepend">{children[0]}</div>;
+		}
 		return (
-			<div className="cube-input">
-				<div className="cube-input-prepend" />
-				<input className="cube-input-field" />
+			<div className={inputClass}>
+				{prepend}
+				<input ref={this.inputRef} className="cube-input-field" onFocus={(e) => this.handleFocus(e)} />
 				<div className="cube-input-append">
 					<div className="cube-input-clear">
 						<i className="cubeic-wrong" />
