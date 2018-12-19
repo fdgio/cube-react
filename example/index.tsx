448139303e7b16207897b9dad361e7dfc3170d39
huruji <594613537@qq.com>
2018-12-19 23:26:57 +0800

    style: :lipstick: add icon for example

--- example/index.tsx
+++ example/index.tsx
@@ -4,6 +4,7 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 
 import 'normalize.css';
+import '../src/common/stylus/index.styl';
 import './index.styl';
 
 ReactDOM.render(<App />, document.querySelector('#app')! as HTMLElement);
